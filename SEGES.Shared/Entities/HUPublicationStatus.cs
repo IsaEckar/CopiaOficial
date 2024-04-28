59e026ad3cfcc12c4f55d3b268543d9339be428e
IsauraEckar <isauraEckar@gmail.com>
2024-04-27 22:21:44 -0500

    Create Entities

---
+++ SEGES.Shared/Entities/HUPublicationStatus.cs
@@ -0,0 +1,17 @@
+﻿using SEGES.Shared.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEGES.Shared.Entities
+{
+    public class HUPublicationStatus : IEntityWithName
+    {
+        public int HUPublicationStatusId { get; set; }
+
+        [Display(Name = "Estado de Publicación de HU")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; }
+    {
+
+    }
+}
