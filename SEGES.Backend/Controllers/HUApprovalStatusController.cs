7f934345614a277600879df7790cd71d86838ff8
IsauraEckar <isauraEckar@gmail.com>
2024-04-28 00:00:23 -0500

    Create Controllers

---
+++ SEGES.Backend/Controllers/HUApprovalStatusController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEGES.Backend.UnitsOfWork.Interfaces;
+using SEGES.Shared.Entities;
+
+namespace SEGES.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HUApprovalStatusController : GenericController<HUApprovalStatus>
+    {
+        private readonly IHuAppruvalStatusRepository _huAppruvalStatusRepository;
+        public HUApprovalStatusController(IGenericUnitOfWork<HUApprovalStatus> unitOfWork, IHuAppruvalStatusRepository huAppruvalStatusRepository) : base(unitOfWork)
+        {
+            _huAppruvalStatusRepository = huAppruvalStatusRepository;
+        }
+        [HttpGet("full")]
+        public override async Task<IActionResult> GetAsync()
+        {
+            var action = await _huAppruvalStatusRepository.GetAsync();
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var action = await _huAppruvalStatusRepository.GetAsync(id);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ SEGES.Backend/Controllers/HUPrioritiesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEGES.Backend.UnitsOfWork.Interfaces;
+using SEGES.Shared.Entities;
+
+namespace SEGES.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HUPrioritiesController : GenericController<HUPriority>
+    {
+        private readonly IHUPrioritiesRepository _repository;
+        public HUPrioritiesController(IGenericUnitOfWork<HUPriority> unitOfWork, IHUPrioritiesRepository repository) : base(unitOfWork)
+        {
+            _repository = repository;
+        }
+        [HttpGet("full")]
+        public override async Task<IActionResult> GetAsync()
+        {
+            var action = await _repository.GetAsync();
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var action = await _repository.GetAsync(id);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+    }
+}
