fb7dfbd72e8c0891b5845cf13ce5633666d1de84
DIANA KATHERINE VELASQUEZ GUTIERREZ <diana5847@hotmail.com>
2024-04-28 00:12:04 -0500

    Create Repository-Interfaces

---
+++ SEGES.Backend/Repositories/Interfaces/ICountriesRepository.cs
@@ -0,0 +1,19 @@
+﻿using SEGES.Shared.Entities;
+using SEGES.Shared.Responses;
+using SEGES.Shared.DTOs;
+
+namespace SEGES.Backend.Repositories.Interfaces
+{
+    public interface ICountriesRepository
+    {
+        Task<ActionResponse<Country>> GetAsync(int id);
+
+        Task<ActionResponse<IEnumerable<Country>>> GetAsync();
+
+        Task<ActionResponse<IEnumerable<Country>>> GetAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+
+    }
+}
---
+++ SEGES.Backend/Repositories/Interfaces/IHuAppruvalStatusRepository.cs
@@ -0,0 +1,11 @@
+﻿using SEGES.Shared.Entities;
+using SEGES.Shared.Responses;
+
+namespace SEGES.Backend.Repositories.Interfaces
+{
+    public interface IHuAppruvalStatusRepository
+    {
+        Task<ActionResponse<HUApprovalStatus>> GetAsync(int id);
+        Task<ActionResponse<IEnumerable<HUApprovalStatus>>> GetAsync();
+    }
+}
