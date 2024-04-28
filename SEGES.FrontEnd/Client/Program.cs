97e1370b15317399bc590d165780475222d8b60a
DIANA KATHERINE VELASQUEZ GUTIERREZ <diana5847@hotmail.com>
2024-04-28 01:52:55 -0500

    Create Program FrontEnd

---
+++ SEGES.FrontEnd/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SEGES.FrontEnd.Repositories;
+
+namespace SEGES.FrontEnd.Client
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7208/") });
+            builder.Services.AddScoped<IRepository, Repository>();
+            builder.Services.AddSweetAlert2();
+
+            await builder.Build().RunAsync();
+
+        }
+    }
+}
