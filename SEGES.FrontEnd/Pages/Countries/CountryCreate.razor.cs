1ead520c2828d311e42a7cc59ca1491f21aae0b7
IsauraEckar <isauraEckar@gmail.com>
2024-04-28 00:14:19 -0500

    Create Page - Countries

---
+++ SEGES.FrontEnd/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.Metrics;
+
+namespace SEGES.FrontEnd.Pages.Countries
+{
+    public partial class CountryCreate
+    {
+        private Country country = new();
+        private FormWithName<Country>? countryForm;
+
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await repository.PostAsync("/api/countries", country);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message);
+                return;
+            }
+
+            Return();
+
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito");
+            //await sweetAlertService.FireAsync(new SweetAlertOptions
+            //{
+            //    Toast = true,
+            //    Position = SweetAlertPosition.BottomEnd,
+            //    ShowConfirmButton = true,
+            //    Timer = 10000,
+            //    Icon = SweetAlertIcon.Success,
+            //    Title = "Éxito",
+            //    Text = "Registro creado con éxito"
+            //});
+
+        }
+
+        private void Return()
+        {
+            countryForm!.FormPostedSuccessfully = true;
+            navigationManager.NavigateTo("/countries");
+        }
+    }
+}
---
+++ SEGES.FrontEnd/Pages/Countries/CountryEdit.razor.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace SEGES.FrontEnd.Pages.Countries
+{
+    public partial class CountryEdit
+    {
+
+        private Country? country;
+        private FormWithName<Country>? countryForm;
+
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+
+        [EditorRequired, Parameter] public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await repository.GetAsync<Country>($"/api/countries/{Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/countries");
+                }
+                else
+                {
+                    var messsage = await responseHttp.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", messsage, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                country = responseHttp.Response;
+            }
+        }
+
+        private async Task EditAsync()
+        {
+            var responseHttp = await repository.PutAsync("/api/countries", country);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message);
+                return;
+            }
+
+            Return();
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con éxito.");
+        }
+
+        private void Return()
+        {
+            countryForm!.FormPostedSuccessfully = true;
+            navigationManager.NavigateTo("/countries");
+        }
+
+    }
+}
