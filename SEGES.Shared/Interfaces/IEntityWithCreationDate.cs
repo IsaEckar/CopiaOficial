9afba893629e3dd2115388fe6abefa23a0a25170
DIANA KATHERINE VELASQUEZ GUTIERREZ <diana5847@hotmail.com>
2024-04-27 21:58:49 -0500

    Create Entities Folder Shared

---
+++ SEGES.Shared/Interfaces/IEntityWithCreationDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEGES.Shared.Interfaces
+{
+    public class IEntityWithCreationDate
+    {
+        DateTime CreationDate { get; set; }
+    }
+}
