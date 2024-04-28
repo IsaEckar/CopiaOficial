1d5d99776229929600be14498252eac7855fe38b
DIANA KATHERINE VELASQUEZ GUTIERREZ <diana5847@hotmail.com>
2024-04-28 00:05:20 -0500

    Create Implementations-Repository

---
+++ SEGES.Backend/Repositories/Implementations/CountriesRepository.cs
@@ -0,0 +1,96 @@
+﻿using SEGES.Shared.DTOs;
+using SEGES.Shared.Entities;
+using SEGES.Shared.Responses;
+using SEGES.Shared;
+using SEGES.Backend.Helpers;
+using SEGES.Backend.Repositories.Interfaces;
+using SEGES.Backend.UnitsOfWork.Implementations;
+using Microsoft.EntityFrameworkCore;
+
+namespace SEGES.Backend.Repositories.Implementations
+{
+    public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
+    {
+        private readonly DataContext _context;
+
+        public CountriesRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Country>>> GetAsync()
+        {
+            var countries = await _context.Countries
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+            return new ActionResponse<IEnumerable<Country>>
+            {
+                WasSuccess = true,
+                Result = countries
+            };
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Country>>> GetAsync(PaginationDTO pagination)
+        {
+            var queryable = _context.Countries
+                .Include(c => c.States)
+                .AsQueryable();
+
+
+
+            return new ActionResponse<IEnumerable<Country>>
+            {
+                WasSuccess = true,
+                Result = await queryable
+                    .OrderBy(x => x.Name)
+                    .Paginate(pagination)
+                    .ToListAsync()
+            };
+        }
+
+        public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginatioDTO pagination)
+        {
+            var queryable = _context.Countries.AsQueryable();
+
+
+            double count = await queryable.CountAsync();
+            int totalPages = (int)Math.Ceiling(count / pagination.RecordsNumber);
+            return new ActionResponse<int>
+            {
+                WasSuccess = true,
+                Result = totalPages
+            };
+        }
+
+
+        public override async Task<ActionResponse<Country>> GetAsync(int id)
+        {
+            var country = await _context.Countries
+                 .Include(c => c.States!)
+                 .ThenInclude(s => s.Cities)
+                 .FirstOrDefaultAsync(c => c.CountryId == id);
+
+            if (country == null)
+            {
+                return new ActionResponse<Country>
+                {
+                    WasSuccess = false,
+                    Message = "País no existe"
+                };
+            }
+
+            return new ActionResponse<Country>
+            {
+                WasSuccess = true,
+                Result = country
+            };
+        }
+
+        public async Task<IEnumerable<Country>> GetComboAsync()
+        {
+            return await _context.Countries
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+        }
+    }
+}
---
+++ SEGES.Backend/Repositories/Implementations/HuAppruvalStatusRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEGES.Backend.Repositories.Interfaces;
+using SEGES.Backend.UnitsOfWork.Implementations;
+using SEGES.Shared.Entities;
+using SEGES.Shared.Responses;
+using SEGES.Shared;
+
+namespace SEGES.Backend.Repositories.Implementations
+{
+   
+        public class HuAppruvalStatusRepository : GenericRepository<HUApprovalStatus>, IHuAppruvalStatusRepository
+        {
+            private readonly DataContext _context;
+            public HuAppruvalStatusRepository(DataContext context) : base(context)
+            {
+                _context = context;
+            }
+
+            public override async Task<ActionResponse<HUApprovalStatus>> GetAsync(int id)
+            {
+                var status = await _context.HUApprovalStatuses.FindAsync(id);
+                if (status == null)
+                {
+                    return new ActionResponse<HUApprovalStatus>
+                    {
+                        WasSuccess = false,
+                        Message = "Estado no existe"
+                    };
+                }
+                return new ActionResponse<HUApprovalStatus>
+                {
+                    WasSuccess = true,
+                    Result = status
+                };
+            }
+
+            public override async Task<ActionResponse<IEnumerable<HUApprovalStatus>>> GetAsync()
+            {
+                var statuses = await _context.HUApprovalStatuses.ToListAsync();
+                return new ActionResponse<IEnumerable<HUApprovalStatus>>
+                {
+                    WasSuccess = true,
+                    Result = statuses
+                };
+            }
+        }
+    }
