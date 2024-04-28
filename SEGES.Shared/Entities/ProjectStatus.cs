d0c3a6c82f159ac2615662e34302e1010e0ad075
IsauraEckar <isauraEckar@gmail.com>
2024-04-27 22:54:24 -0500

    Create ProjectStatus entity

---
+++ SEGES.Shared/Entities/ProjectStatus.cs
@@ -0,0 +1,21 @@
+﻿using SEGES.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEGES.Shared.Entities
+{
+    public class ProjectStatus : IEntityWithName
+    {
+        public int ProjectStatusId { get; set; }
+
+        [Display(Name = "Estado de Proyecto")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; }
+
+    }
+}
