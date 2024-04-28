4081a5e57db0cba2b1d870ecdd4c10fecf9c2f64
IsauraEckar <isauraEckar@gmail.com>
2024-04-28 00:25:03 -0500

    Create Page - HUApprovalStatuses

---
+++ SEGES.FrontEnd/Pages/HUApprovalStatuses/HUApprovalStatusCreate.razor.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components;
+using SEGES.Shared.Entities;
+
+namespace SEGES.FrontEnd.Pages.HUApprovalStatuses
+{
+    public partial class HUApprovalStatusCreate
+    {
+        private HUApprovalStatus approvalStatus = new();
+        private FormWithName<HUApprovalStatus>? approvalStatusForm;
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await Repository.PostAsync("/api/HUApprovalStatus", approvalStatus);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message);
+                return;
+            }
+
+            Return();
+
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito");
+        }
+
+
+
+
+        private void Return()
+        {
+            approvalStatusForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("/HUApprovalStatus");
+        }
+    }
+}
