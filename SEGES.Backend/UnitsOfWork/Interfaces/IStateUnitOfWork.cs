a4697e9897a9774f628b607c5ef02ee601443941
MelissaArcila <restrepo.melissa@gmail.com>
2024-04-27 23:58:20 -0500

    Creation of the interfaces for the workunits

---
+++ SEGES.Backend/UnitsOfWork/Interfaces/IStateUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using SEGES.Shared.Entities;
+using SEGES.Shared.Responses;
+using SEGES.Shared.DTOs;
+
+namespace SEGES.Backend.UnitsOfWork.Interfaces
+{
+    public interface IStateUnitOfWork
+    {
+        Task<ActionResponse<State>> GetAsync(int id);
+
+        Task<ActionResponse<IEnumerable<State>>> GetAsync();
+
+        Task<ActionResponse<IEnumerable<State>>> GetAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+    }
+}
