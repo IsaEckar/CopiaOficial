861ce32201e13419f817e2d53358e53e2d1137e5
IsauraEckar <isauraEckar@gmail.com>
2024-05-06 02:41:08 -0500

    Create User

--- SEGES.Shared/Entities/User.cs
+++ SEGES.Shared/Entities/User.cs
@@ -1,36 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Identity;
+using SEGES.Shared.Enums;
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SEGES.Shared.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserId { get; set; }
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; } = null!;
 
-        [Display(Name = "Usuario")]
-        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
-        public string Name { get; set; } = null!;
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; } = null!;
 
-        [Display(Name = "Correo Electrónico")]
-        [MaxLength(254, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
-        public string EmailAddress { get; set; }
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; } = null!;
+
+        [Display(Name = "Dirección")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Address { get; set; } = null!;
+
+        [Display(Name = "Foto")]
+        public string? Photo { get; set; }
+
+        [Display(Name = "Tipo de usuario")]
+        public UserType UserType { get; set; }
+
+        public City? City { get; set; }
 
         [Display(Name = "Ciudad")]
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
-        public int City_Id { get; set; }
-        public City City { get; set; } = null!;
+        [Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar una {0}.")]
+        public int CityId { get; set; }
 
-        [Display(Name = "ID Rol")]
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
-        public int Role_ID { get; set; }
-        public Role Role { get; set; } = null!;
-
-        public ICollection<Role> Roles { get; set; }
+        [Display(Name = "Usuario")]
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
---
+++ SEGES.Shared/Enums/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace SEGES.Shared.Enums
+{
+    public enum UserType
+    {
+        [Description("Administrador")]
+        Admin,
+
+        [Description("Usuario")]
+        User
+    }
+}
