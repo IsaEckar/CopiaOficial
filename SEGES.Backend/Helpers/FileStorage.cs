89fa302226dcbad74571f24adc94440238aedbe1
IsauraEckar <isauraEckar@gmail.com>
2024-05-09 22:01:33 -0500

    Change to Cloudinary

--- SEGES.Backend/Helpers/FileStorage.cs
+++ SEGES.Backend/Helpers/FileStorage.cs
@@ -20,8 +20,7 @@ namespace SEGES.Backend.Helpers
 
         public async Task RemoveFileAsync(string path, string containerName)
         {
-            // No es necesario para Cloudinary, ya que Cloudinary se encarga de la gestión de archivos.
-            // Puedes implementar este método según sea necesario.
+           
         }
 
         public async Task<string> SaveFileAsync(byte[] content, string extention, string containerName)
@@ -31,10 +30,10 @@ namespace SEGES.Backend.Helpers
                 var uploadParams = new ImageUploadParams()
                 {
                     File = new FileDescription(Guid.NewGuid().ToString(), stream),
-                    PublicId = null, // Cloudinary genera automáticamente un PublicId único si no se proporciona uno
-                    Folder = containerName, // Opcional: Puedes especificar una carpeta en Cloudinary para almacenar los archivos
-                    Format = extention.Replace(".", ""), // La extensión debe ser sin el punto
-                    Overwrite = false, // Si ya existe un archivo con el mismo nombre, no lo sobrescribe
+                    PublicId = null,
+                    Folder = containerName, 
+                    Format = extention.Replace(".", ""), 
+                    Overwrite = false, 
                 };
 
                 var uploadResult = await _cloudinary.UploadAsync(uploadParams);
